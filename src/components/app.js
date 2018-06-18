219940d7115918e69d707cad9d8226c17f320499
dburns1 <patriciadburns@gmail.com>
2018-06-17 22:09:29 -0700

    react practice

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,10 @@
 
+import React, {Component} from 'react'; 
 
+class App extends Component {
+    render(){
+        return React.createElement('div', null, 'h1', "Hello from React")
+    }
+}
+
+export default App; 
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from 'app.js'; 
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">Patricia Burns</h1>, 
+    <App/>, 
     document.getElementById('root')
 );
